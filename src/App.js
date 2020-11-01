8e4c4f9a8ecb071782803762ca5328537c82832c
Lorenzo <lorenzozar@gmail.com>
2020-11-01 22:00:11 +0200

    basic style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <nav className="navbar">
+        Space reserved for navbar
+    </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        YOUR HTML QUIZ
       </header>
+      <div className='main-section'>
+        <div className="question-container">
+          <h2>What is...</h2>
+        </div>
+        <div className="answers-container">
+          <div className="answer-container">Answer A</div>
+          <div className="answer-container">Answer B</div>
+          <div class="break"></div>
+          <div className="answer-container">Answer C</div>
+          <div className="answer-container">Answer D</div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
